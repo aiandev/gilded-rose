be772a4d4b2968ef9d277df68b800fb259beead6
AIAN <mdsy179@gmail.com>
2026-02-09 15:59:55 +0100

    fix: correct quality decrement for Conjured items and update related tests

--- app/updaters/aged-brie.ts
+++ app/updaters/aged-brie.ts
@@ -1,5 +1,4 @@
 import { Item } from "@/gilded-rose";
-import { MAX_QUALITY } from "../constants/quality";
 import { decrementSellIn, increaseQuality, isExpired } from "@/helper";
 
 export function updateAgedBrie(item: Item): void {
--- app/updaters/conjured.ts
+++ app/updaters/conjured.ts
@@ -2,10 +2,10 @@ import { Item } from "@/gilded-rose";
 import { decrementQuality, decrementSellIn, isExpired } from "@/helper";
 
 export function updateConjured(item: Item): void {
-  decrementQuality(item, 1);
+  decrementQuality(item, 2);
 
   decrementSellIn(item);
   if (isExpired(item)) {
-    decrementQuality(item, 1);
+    decrementQuality(item, 2);
   }
 }
